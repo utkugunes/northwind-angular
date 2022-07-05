859be0712ec6a0ddcc327d685a4ab09abe9f6a95
utku güneş <utkugunes@outlook.com>
2022-07-05 23:47:17 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'northwind';
+  product:string = "test";
+
+  product1={productId:1,productName:'laptop', categoryId:'1', unitPrice:5}
+  product2={productId:2,productName:'mouse', categoryId:'1', unitPrice:5}
+  product3={productId:3,productName:'keyboard', categoryId:'1', unitPrice:5}
+  product4={productId:4,productName:'monitor', categoryId:'1', unitPrice:5}
+  product5={productId:5,productName:'camera', categoryId:'1', unitPrice:5}
+
+  products=[this.product1, this.product2,this.product3, this.product4,this.product5];
+
 }
